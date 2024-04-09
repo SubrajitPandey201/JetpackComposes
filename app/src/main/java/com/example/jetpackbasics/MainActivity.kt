62de9b499029d923b5a0711889ef5402b3116fd7
SubrajitPandey201 <intern5-tc@uidai.net.in>
2024-04-09 11:51:18 +0530

    jetpack compose with composable function ----->
    how to change existing project into jetpack compose !
    
    how to render the preview using jetpack -->

--- app/src/main/java/com/example/jetpackbasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackbasics/MainActivity.kt
@@ -3,44 +3,34 @@ package com.example.jetpackbasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackbasics.ui.theme.JetpackBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            //passing composable here
+            Text(text = "Hello to the jetpack compose")
+
         }
     }
 }
-
+@Preview(showBackground = true, name = "hello message")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SayCheesy(name: String="CheesyCode"){
+    Text(text = "SaY Cheesy bOys from $name ")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "hello message 2", showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    JetpackBasicsTheme {
-        Greeting("Android")
-    }
+fun SayCheesy2(name: String="CheesyCode"){
+    Text(text = "SaY Cheesy bOys from $name ")
+}
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+fun PreviewFunction(){
+    SayCheesy(name = "Hello everyone")
+
 }
